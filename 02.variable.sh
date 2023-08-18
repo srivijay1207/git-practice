558c16a1c803d936ce78007367f5bec586ce1286
srivijay <srivijay1207@gmail.com>
2023-08-18 23:36:06 +0530

    2nd change

---
+++ 02.variable.sh
@@ -0,0 +1,34 @@
+#1/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+echo "pls login with root user"
+exit 10
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ] ; then
+
+echo "mqsql install failed"
+exit 1
+
+else 
+ echo "mysql installation is success"
+
+fi
+
+yum install nginx -y
+
+if [ $? -ne 0 ] ; then
+
+echo "nginx install failed "
+
+exit 10
+else 
+
+echo " installed NGINX...!"
+
+fi
