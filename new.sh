58e075d13b10a80457633835cdcea2b878458dd8
srivijay <srivijay1207@gmail.com>
2023-08-20 17:15:35 +0530

    word in line

--- new.sh
+++ new.sh
@@ -1,11 +1,11 @@
 #!bin/bash
-f-name="a1,txt"
+f-name="a1.txt"
 
 content="we are writing code for searching words ramesh suresh"
 
-echo"$content" > "$f-name"
+echo "$content" > "$f-name"
 
-if [ -e $f-name]; then
+if [ -e $f-name ]; then
 
 echo "file $f-name is created"
 cat $f-name
