912e3e8ec5356055a2537e135f1ffc87c9deb543
srivijay <srivijay1207@gmail.com>
2023-08-20 05:54:16 +0530

    function 2nd line

--- function.sh
+++ function.sh
@@ -8,16 +8,17 @@
 # #echo "i am vijay"
 #  Hello $1 $2
 
-USERID=$(id -u)
+USERID=$ (id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "pls login with root user....?"
     exit 1
 fi
-yum install mysql -y
+
+    yum install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "installation Mysql failed"
 else
-    echo" instalation Mysql is success....!!!"
+    echo "instalation Mysql is success....!!!"
 fi
