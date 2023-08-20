32cd744eb399648b2c5a28d3fa2b7cdd60db955b
srivijay <srivijay1207@gmail.com>
2023-08-20 05:52:17 +0530

    function 2nd line

--- function.sh
+++ function.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-Hello (){
-    echo " welcome to Devops:$1 $2"
+# Hello (){
+#     echo " welcome to Devops:$1 $2"
 
-}
+# }
 
-#echo "i am vijay"
- Hello $1 $2
+# #echo "i am vijay"
+#  Hello $1 $2
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "pls login with root user....?"
+    exit 1
+fi
+yum install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "installation Mysql failed"
+else
+    echo" instalation Mysql is success....!!!"
+fi
